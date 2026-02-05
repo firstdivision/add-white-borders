1412dea45cf7cc9e769f1f3ccb5a63c906c7dea4
killfly <realkillfly@gmail.com>
2026-02-04 21:45:17 -0600

    Fix deploy fails becaue of vite base

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const baseFromEnv = process.env.VITE_BASE
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: repoName ? `/${repoName}/` : '/',
+  base: baseFromEnv ?? (repoName ? `/${repoName}/` : '/'),
   plugins: [react()],
 })
